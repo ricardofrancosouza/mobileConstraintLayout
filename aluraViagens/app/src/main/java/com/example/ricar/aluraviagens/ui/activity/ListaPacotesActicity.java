7f72e29074660290ed97dae6c97a9d0b442bb086
DESKTOP-JTBQR68\ricar <ricardofranco>
2018-07-02 23:07:36 -0300

    desenvolvimento até a aula 07

---
+++ aluraViagens/app/src/main/java/com/example/ricar/aluraviagens/ui/activity/ListaPacotesActicity.java
@@ -0,0 +1,15 @@
+package com.example.ricar.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ricar.aluraviagens.R;
+
+public class ListaPacotesActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes_activity);
+    }
+}
